1e623b3331e327b3c3d87352f5d46e4a89adee2f
MacBook <macbook@MacBook-Pro-de-MacBook.local>
2021-04-04 18:32:11 +0000

    tp1 calculatrice

--- TP1/ViewController.swift
+++ TP1/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var field1: UITextField!
+    @IBOutlet weak var field2: UITextField!
+    
+    @IBOutlet weak var result: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func onClick(_ sender: UIButton) {
+        
+        result.text="resultat :"
+        
+        let nb1 = Double(field1.text!) ?? 0
+        let nb2 = Double(field2.text!) ?? 0
+        
+        let sum = nb1 + nb2
+        
+        result.text = "\(String(describing: result.text!))"+"\(sum)"
+        
+    }
+    
+    
+    
+    
 }
 
